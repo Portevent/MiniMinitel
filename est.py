6c71a84dff737a8250141d3b6b33ddab9d296df2
Portevent <porte@magous.fr>
2026-01-12 20:23:20 +0100

    feat : remove old asset

--- est.py
+++
@@ -1,9 +0,0 @@
-from minitel.minitel import Minitel
-
-with Minitel() as minitel:
-    for i in range(1, 40):
-        for j in range(1, 20):
-
-            # minitel.clear()
-            minitel.cursorMove(i, j)
-            minitel.write(" Zouze de love")
--- test4.py
+++
@@ -1,5 +0,0 @@
-from minitel.minitel_image import MinitelImage
-
-with MinitelImage() as minitel:
-    minitel._mixte_to_videotex()
-    minitel.showImageBuggy("asset/logo recreated.png", "center")
